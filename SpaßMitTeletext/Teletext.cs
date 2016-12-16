8889283389b6fb95935d5bf8c572a9b846ee0c3d
Matthew <Matthew@FLASH32>
2016-12-16 13:08:41 +0100

    Teletext get and set

--- SpaßMitTeletext/Teletext.cs
+++ SpaßMitTeletext/Teletext.cs
@@ -8,6 +8,73 @@ namespace SpaßMitTeletext
 {
     class Teletext
     {
+        public enum NR {111, 222, 333, 444, 555 };
+
         private string Name;
+        private string desc;
+        private NR nr;
+        private DateTime date;
+
+        public Teletext(string name, string desc, int nr, DateTime date)
+        {
+            this.Name1=name;
+            this.Desc = desc;
+            this.Nr = nr;
+            this.Date = date;
+        }
+
+        public string Name1
+        {
+            get
+            {
+                return Name;
+            }
+
+            set
+            {
+                Name = value;
+            }
+        }
+
+        public string Desc
+        {
+            get
+            {
+                return desc;
+            }
+
+            set
+            {
+                desc = value;
+            }
+        }
+
+        public NR Nr
+        {
+            get
+            {
+                return nr;
+            }
+
+            set
+            {
+                nr = value;
+            }
+        }
+
+        public DateTime Date
+        {
+            get
+            {
+                return date;
+            }
+
+            set
+            {
+                date = value;
+            }
+        }
+
+
     }
 }
